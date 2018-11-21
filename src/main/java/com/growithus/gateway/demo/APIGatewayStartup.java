acda2dde81a60b53b9c6ed88ce78b45d29d10352
apple <apple@localhost>
2018-11-21 14:30:58 +0800

    initial api gateway demo project

---
+++ src/main/java/com/growithus/gateway/demo/APIGatewayStartup.java
@@ -0,0 +1,32 @@
+/**
+ * Project Name:api-gateway-demo
+ * File Name:APIGatewayStartup.java
+ * Package Name:com.growithus.gateway.demo
+ * Date:2018年11月21日下午2:28:22
+ *
+*/
+
+package com.growithus.gateway.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName:APIGatewayStartup <br/>
+ * Function: <br/>
+ * Date: 2018年11月21日 下午2:28:22 <br/>
+ * 
+ * @author apple
+ * @version
+ * @since JDK 1.6
+ * @see
+ */
+@SpringBootApplication
+public class APIGatewayStartup {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(APIGatewayStartup.class, args);
+    }
+
+}
